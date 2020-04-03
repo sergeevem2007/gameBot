16c759790ec0eedae151098309c548e4c386308d
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-04-03 18:49:11 +0400

    feat: add gameFunc

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+let isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+alert('Угадай число от 1 до 100');
+
+
+function gameFunc() {
+  const answer = parseInt(Math.random() * 100);
+  console.log(answer);
+  return function checkQuestion() {
+    console.log(answer);
+    let question = prompt('Введи число!');
+    console.log(isNumber(question));
+    if (question === null) {
+      alert('Игра закончена');
+    } else if (!isNumber(question)) {
+      checkQuestion();
+    } else if (+question > answer) {
+      alert('Загаданное число меньше');
+      checkQuestion();
+    } else if (+question < answer) {
+      alert('Загаданное число больше');
+      checkQuestion();
+    } else {
+      alert('Поздравляю, Вы угадали!!!');
+    }
+  };
+};
+const game = gameFunc();
+game();
